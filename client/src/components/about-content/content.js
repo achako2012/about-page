58dca70ef7e27ab3ee520d22dcec634b84385927
aleksander.chako <aleksander.chako@kindgeek.com>
2021-07-10 10:05:36 +0300

    small fixes

--- client/src/components/about-content/content.js
+++ client/src/components/about-content/content.js
@@ -1,4 +1,5 @@
 import './title.css'
+import './about.css'
 
 const Title = () => {
     return (
@@ -27,13 +28,24 @@ const Title = () => {
 const About = () => {
     return (
         <section className='about-section'>
-            <div className='about-photo'>
-                <img/>
+            <div className='row'>
+                <div className='about'>
+                    <div className='about-photo'>
+                        <h1>Photo</h1>
+                    </div>
+                    <div className='about-info'>
+                        <h1 className='about-title'>about me</h1>
+                        <p className='about-article'>Knowledgeable, self-driven, and certified
+                            Automation
+                            Test
+                            Engineer skilled at developing new
+                            tests and test plans, participating in all facets of project development, and performing
+                            risk
+                            analysis</p>
+                    </div>
+                </div>
             </div>
-            <div className='about-info'>
-                <h1 className='about-title'>hello,</h1>
-                <p className='about-article'>text here</p>
-            </div>
+
         </section>
     )
 }
